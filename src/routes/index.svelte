79eaf2a773fd422f5b6467cf21eb7cf86979c89d
aircrftdev <dev+github@aircrft.com>
2021-07-15 03:00:23 +0000

    Commit

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -17,10 +17,15 @@ $: {
 fetchPokemon();
 </script>
 <svelte:head>
-	<title>Pokedex</title>
+	<title>Aircrft Problem / Solution Stack</title>
 </svelte:head>
 
-<h1 class="text-4xl text-center my-8 uppercase">SvelteKit Pokedex</h1>
+<h1 class="text-4xl text-center my-8 uppercase">Aircrft Problem / Solution Stack</h1>
+<p>Welcome the Aircrft powered Problem / Solution Stack - the world's most up-to-the minute realtime open source problem and solution database. "Craigslist for problem solving!"</p>
+
+<br>
+<br>
+<p>Organized in 4 core problem sets: Urgent, Future, Common, and anything else.</p>
 <input class="w-full rounded-md text-lg p-4 border-2 border-gray-200" bind:value={searchTerm} placeholder="Search Pokemon">
 <div class="py-4 grid gap-4 md:grid-cols-2 grid-cols-1">
     
@@ -28,4 +33,8 @@ fetchPokemon();
             <PokemanCard pokeman={pokeman}/>
     {/each}
 </div>
-    
+<style>
+    h1 {
+        color: rgb(44, 37, 148);
+    }
+</style> 
